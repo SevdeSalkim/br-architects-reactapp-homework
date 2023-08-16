6dd1f8b5aa148a0472066a1825a45ad4d5dd45ce
SevdeSalkim <sevde.salkim@gmail.com>
2023-08-17 01:37:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import Navbar from "./components/Navbar"
+import Header from "./components/Header"
+import Content from "./components/content/Content"
+import Footer from "./components/Footer"
+
+
+
+function App() {
+  return (
+   <>
+   <Navbar/>
+   <Header/>
+   <Content/>
+   <Footer/>
+
+   
+   
+   
+   </>
+  );
+}
+
+export default App;
